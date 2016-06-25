1f9df9e732724aba27895614a3be8e8f44f86418
LXF <rdxer@foxmail.com>
2016-06-25 23:31:23 +0800

    Initial Commit

---
+++ iOS_XXLibrary/iOS_XXLibrary.h
@@ -0,0 +1,19 @@
+//
+//  iOS_XXLibrary.h
+//  iOS_XXLibrary
+//
+//  Created by LXF on 16/6/25.
+//  Copyright © 2016年 LXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS_XXLibrary.
+FOUNDATION_EXPORT double iOS_XXLibraryVersionNumber;
+
+//! Project version string for iOS_XXLibrary.
+FOUNDATION_EXPORT const unsigned char iOS_XXLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_XXLibrary/PublicHeader.h>
+
+
